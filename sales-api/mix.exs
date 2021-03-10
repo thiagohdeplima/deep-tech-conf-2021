268a9b2d02ab1714e53969be962b1130617aee4b
Thiago Henrique de Paulo Lima <thiago.lima@semantix.com.br>
2021-03-10 00:12:07 -0300

    chore: install amqp

--- sales-api/mix.exs
+++ sales-api/mix.exs
@@ -42,7 +42,9 @@ defmodule Sales.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      {:amqp, "~> 2.1"}
     ]
   end
 
